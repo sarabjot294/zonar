b0a7791a82f7e25ee6de77ea8f700c015d5208bd
sarabjot294 <sarabot294@gmail.com>
2021-11-16 01:15:37 -0800

    Added tests for APIs

---
+++ tests/Feature/WishlistTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class WishlistTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    //Make sure all the values are correct. Else, restore the db and then run tests.
+
+    public function test_success_add_wishlist()
+    {
+        $formData = [
+            'user_id' => 1,
+            'book_id' => 5
+        ];
+        $response = [
+            'status' => true        
+        ];
+        $this->post('http://127.0.0.1:8000/api/wishlist',$formData)
+            ->assertStatus(200)
+            ->assertJson($response);
+    }
+
+    public function test_failure_add_wishlist()
+    {
+        $formData = [
+            'user_id' => 0,
+            'book_id' => 5
+        ];
+        $response = [
+            'status' => false        
+        ];
+        $this->post('http://127.0.0.1:8000/api/wishlist',$formData)
+            ->assertStatus(200)
+            ->assertJson($response);
+    }
+
+    public function test_success_update_wishlist()
+    {
+        $formData = [
+            'user_id' => 2,
+            'book_id' => 5
+        ];
+        
+        $response = [
+            'status' => true        
+        ];
+
+        $this->put('http://127.0.0.1:8000/api/wishlist/10',$formData)
+            ->assertStatus(200)
+            ->assertJson($response);
+    }
+
+    public function test_failure_update_wishlist()
+    {
+        $formData = [
+            'user_id' => 2,
+            'book_id' => 7
+        ];
+        
+        $response = [
+            'status' => false        
+        ];
+
+        $this->put('http://127.0.0.1:8000/api/wishlist/10',$formData)
+            ->assertStatus(200)
+            ->assertJson($response);
+    }
+
+    public function test_success_delete_wishlist_entry()
+    {   
+        $response = [
+            'status' => true        
+        ];
+
+        $this->delete('http://127.0.0.1:8000/api/wishlist/11')
+            ->assertStatus(200)
+            ->assertJson($response);
+    }
+
+    public function test_failure_delete_wishlist_entry()
+    {   
+        $response = [
+            'status' => false        
+        ];
+
+        $this->delete('http://127.0.0.1:8000/api/wishlist/100')
+            ->assertStatus(200)
+            ->assertJson($response);
+    }
+}
